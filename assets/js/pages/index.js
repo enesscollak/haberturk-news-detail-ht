66a38567c2278680d57abf0128f28fb7b042344c
ENES COLAK <enescolak@cyh.local>
2021-12-29 16:21:18 +0300

    update

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,5 +1,11 @@
 console.log("js loaded");
 
+//RELATED NEWS
+var bottom = new Swiper(".related-news .related-swiper", {
+  slidesPerView: "auto",
+  loop: true,
+})
+
 //GÜNDEMDEN HABERLER
 var swiper = new Swiper(".journal-news .wrapper .swiper", {
   pagination: {
@@ -28,7 +34,8 @@ var bottom = new Swiper(".category-widget .swiper", {
 //YUKARI KAYDIR
 var scrollToTopBtn = document.getElementById("scrollToTopBtn");
 var rootElement = document.documentElement;
-function scrollToTop (){
+
+function scrollToTop() {
   rootElement.scrollTo({
     top: 0,
     behavior: "smooth"
@@ -39,17 +46,20 @@ scrollToTopBtn.addEventListener("click", scrollToTop);
 
 //YORUMLAR
 var reply_Btn = document.querySelectorAll('.comment-area .item .buttons .btn-reply');
-var message = document.querySelectorAll('.comment-area .item .popup');
+var message = document.querySelectorAll('.comment-area .item');
 
-function itemMessageBox() {
-  message.forEach(element => {
-    element.classList.remove("show");
+function itemMessageBox(ikeyNumber) { //Gelen ikey'i message içindeki jkey numarası ile kontrol edilir.
+  message.forEach((j, jkey) => {
+    if (ikeyNumber == jkey) {// Eğer keyler eşitse açar ya da kapar
+      j.classList.toggle("show");
+    } else {
+      j.classList.remove("show"); // Eğer keyler eşit değilse class silinir ve message kapanır
+    }
   });
 }
 
-reply_Btn.forEach(element => {
-    element.addEventListener("click", function(){
-      itemMessageBox();
-      element.closest(".item").querySelector(".popup").classList.toggle("show");
-    });
+reply_Btn.forEach((i, ikey) => {// i: selektör, ikey: index numarası
+  i.addEventListener("click", function () { //i selektörüne click olduğunda ikey numarasını fonksiyona gönderir.
+    itemMessageBox(ikey);
+  });
 });
